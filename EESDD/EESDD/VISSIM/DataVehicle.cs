a949559bd575db5c21ed8a33854d3e53a1dcd322
AllenLyu <allen.walker.lc@gmail.com>
2015-04-24 12:27:08 +0800

    add VISSIM contrl

---
+++ EESDD/EESDD/VISSIM/DataVehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EESDDTEST.VISSIM
+{
+    class DataVehicle
+    {
+        private double speed;
+
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        private int lane;
+
+        public int Lane
+        {
+            get { return lane; }
+            set { lane = value; }
+        }
+        const double desirSpeed = -1000.0;
+
+        public double DesirSpeed
+        {
+            get { return desirSpeed; }
+        } 
+
+    }
+}
