764dde9cd53777669a05c93cded7d4cc0b95e1b7
AllenLyu <allen.walker.lc@gmail.com>
2015-05-02 21:10:56 +0800

    add sql part

---
+++ EESDD/EESDD/DB/AccessDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using EESDD.Control.User;
+namespace EESDDTEST.DB
+{
+    class AccessDB
+    {
+        OleDbConnection mycon = null;
+        OleDbDataReader myReader = null;
+        public AccessDB(string path)
+        {
+            string strcon = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source = " + @path;
+            mycon = new OleDbConnection(strcon);
+            mycon.Open();
+        }
+        public void insertData(User save)
+        {
+            string sql = "insert into [User] (usrName,usrType,experiencePath) values('" + save.Name + "'," + save.UserClass + ",'" + save.Experiences + "')";
+            OleDbCommand mycom = new OleDbCommand(sql, mycon);
+            mycom.ExecuteReader();
+        }
+
+        public User getData(string name)
+        {
+            string sql = "SELECT * FROM [User] WHERE usrName = '"+name+"'";
+            OleDbCommand mycom = new OleDbCommand(sql, mycon);
+            myReader = mycom.ExecuteReader();
+            User outData = new User();
+            outData.Name = name;
+            outData.UserClass = myReader.GetInt32(2);
+            outData.Experiences = myReader.GetString(3);
+            return outData;
+        }
+    }
+}
